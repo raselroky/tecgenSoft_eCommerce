d4ad3341765942e20761d405a112ab73834512bb
raselroky <raselroky016@gmail.com>
2024-08-31 13:35:05 +0600

    added campaign+deals with products

--- configure/admin.py
+++ configure/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Country,Banner
+from .models import Country,Banner,PlatformCoupon,AllUsedCoupon,MultipleAddress
 
 admin.site.register(Country)
 admin.site.register(Banner)
+admin.site.register(PlatformCoupon)
+admin.site.register(AllUsedCoupon)
+admin.site.register(MultipleAddress)
