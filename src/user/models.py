a96d91410067a3a30d6a88438eea58eef10cbf41
raselroky <raselroky016@gmail.com>
2024-09-10 09:01:34 +0600

    new user

--- src/user/models.py
+++ src/user/models.py
@@ -44,7 +44,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['email']
+    REQUIRED_FIELDS = ['email','date_of_birth']
     objects = UserManager()
 
     class Meta:
