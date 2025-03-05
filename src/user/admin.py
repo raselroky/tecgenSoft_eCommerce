99a9a926557342323360d5f5e51108c2e4ac1a8a
raselroky <raselroky016@gmail.com>
2025-03-05 16:11:52 +0600

    users done

--- src/user/admin.py
+++ src/user/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from user.models import User,UserAddress
+from user.models import *
 
 
 class UsersAdminColumn(admin.ModelAdmin):
@@ -19,3 +19,23 @@ class UserAddressAdminColumn(admin.ModelAdmin):
     def get_list_display_links(self, request, list_display):
         return list_display 
 admin.site.register(UserAddress,UserAddressAdminColumn)
+
+class RolesAdminColumn(admin.ModelAdmin):
+    def get_list_display(self, request):
+        return [field.name for field in self.model._meta.fields]
+    
+    # Make all fields clickable
+    def get_list_display_links(self, request, list_display):
+        return list_display 
+admin.site.register(Roles,RolesAdminColumn)
+
+class RolesPermissionAdminColumn(admin.ModelAdmin):
+    def get_list_display(self, request):
+        return [field.name for field in self.model._meta.fields]
+    
+    # Make all fields clickable
+    def get_list_display_links(self, request, list_display):
+        return list_display 
+admin.site.register(RolePermissions,RolesPermissionAdminColumn)
+
+
--- src/user/serializers.py
+++ src/user/serializers.py
@@ -1,6 +1,17 @@
 from rest_framework import serializers
-from user.models import User,UserAddress
+from user.models import User,UserAddress,Roles,RolePermissions
+from django.contrib.auth.models import Permission
 
+
+class UsersSerializer2(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True,required=False)
+    role = serializers.PrimaryKeyRelatedField(queryset=Roles.objects.all(), many=True, required=False, allow_null=True)
+    email=serializers.CharField(required=False)
+    class Meta:
+        model = User
+        fields = ('id', 'username','email', 'first_name', 'last_name', 'photo', 'is_superadmin', 'is_active', 'gender', 'password', 'permissions', 'role')
+
+    
 
 class UserSerializer(serializers.ModelSerializer):
     username = serializers.CharField(required=True)
@@ -32,4 +43,46 @@ class UserAddressSerializer(serializers.ModelSerializer):
         model = UserAddress
         fields = '__all__'
 
+    
+class RolesSerializer(serializers.ModelSerializer):
+    #permissions = serializers.SerializerMethodField()
+    class Meta:
+        model = Roles
+        fields = "__all__"
 
+class PermissionSerializer2(serializers.ModelSerializer):
+    class Meta:
+        model = Permission
+        fields = "__all__"
+class RolePermissionsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RolePermissions
+        fields = "__all__"
+
+class RolePermissionSerializer2(serializers.ModelSerializer):
+    permission = serializers.PrimaryKeyRelatedField(queryset=Permission.objects.all(), many=True)
+
+    class Meta:
+        model = RolePermissions
+        fields = ['id', 'role', 'permission']
+
+    def create(self, validated_data):
+        permissions = validated_data.pop('permission')  # Get permissions
+        role_permissions = RolePermissions.objects.create(**validated_data)  # Create RolePermissions instance
+        role_permissions.permission.set(permissions)  # Add the permissions to the instance
+        role_permissions.save()
+        return role_permissions
+    
+class RolePermissionSerializerModify(serializers.ModelSerializer):
+    permission = PermissionSerializer2(many=True)
+    role=serializers.SerializerMethodField()
+    def get_role(self,obj):
+        if obj.role:
+            return {
+                "id":obj.role.id,
+                "role":obj.role.title
+            }
+        return None
+    class Meta:
+        model = RolePermissions
+        fields = ['id', 'role', 'permission']
